8a07f63dda2f6b2f3033b669287c2f2433ada033
WitoldDankiewicz <witold.dankiewicz98@gmail.com>
2022-04-07 14:39:20 +0200

    Database connection validation.

--- Database/database.py
+++ Database/database.py
@@ -1,5 +1,15 @@
-from pymongo import MongoClient
+from pymongo import MongoClient,errors
 
-client = MongoClient('mongo', 27017)
+
+client = MongoClient('localhost', 27017, serverSelectionTimeoutMS=4000)
+try:
+    client.server_info()
+except errors.ServerSelectionTimeoutError:
+    try:
+        client = MongoClient('mongo', 27017, serverSelectionTimeoutMS=4000)
+    except errors.ServerSelectionTimeoutError:
+        print("Could not connect to database. App will shut down.")
+        exit()
+
 question_answer_collection = client['PollApp']['QuestionAnswer']
 feedback_collection = client['FeedbackApp']['Feedback']
